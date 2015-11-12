34c2a8f7fd8280866e8e77226e8f0492edc5d0cd
Yunfei Chen <ychen@ooyala.com>
2015-11-11 23:28:56 -0800

    fix bug

--- homeinfo.py
+++ homeinfo.py
@@ -4,74 +4,50 @@ from bs4 import BeautifulSoup
 
 
 def readURL(url):
-	return str(urllib.request.urlopen(url).read())
+	return urllib.request.urlopen(url).read().decode("utf-8")
 	
 
-garden_urls = "http://faq.gardenweb.com/faq/" #garden FAQ page
-home_urls = "http://ths.gardenweb.com/faq/" #home FAQ page
-htmlfile_g = urllib.request.urlopen(garden_urls)
-htmltext_g = str(htmlfile_g.read())
-htmlfile_h = urllib.request.urlopen(home_urls)
-htmltext_h = str(htmlfile_h.read())
+garden_url = "http://faq.gardenweb.com/faq/" #garden FAQ page
+home_url = "http://ths.gardenweb.com/faq/" #home FAQ page
+garden_html = readURL(garden_url)
+home_html = readURL(home_url)
 
-# test 
-# htmltext = '<a class="topic-link mts" href="http://faq.gardenweb.com/forums/butterfly-garden-faq">Butterfly Garden</a>'
-#regex1 = '<a class=\"topic-link mts\" href=\"(.*?)\">(.*?)</a>' 
-#pattern1 = re.compile(regex1)
-#urlpair_g = re.findall(pattern1,htmltext_g)
-
-
-regex2 = '<a class=\"topic-link mts\" href=\"(.*?)\">.*?</a>' # topic links
+regex2 = '<a class="topic-link mts" href="(.*?)">' # topic links
 pattern2 = re.compile(regex2)
-urllist_g = re.findall(pattern2,htmltext_g) # topic links in garden FAQ
-urllist_h = re.findall(pattern2,htmltext_h) # topic links in home FAQ
+urllist_g = re.findall(pattern2, garden_html) # topic links in garden FAQ
+urllist_h = re.findall(pattern2, home_html) # topic links in home FAQ
 
 questions = []
 question_urls = []
 
 # home FAQ, topic means category
 for topic_url in urllist_h:
-	htmlfile = urllib.request.urlopen(topic_url)
-	htmltext = str(htmlfile.read())
-	regex_question = '<span class=\'questionTitle\'>(.*?)</span>'
+	htmltext = readURL(topic_url)
+	regex_question = "<span class='questionTitle'>(.*?)</span>"
 	pattern_question = re.compile(regex_question)
 	quests = re.findall(pattern_question, htmltext)
-	#print(quests)
 	questions.extend(quests)
 
-	regex_question_urls = '<a class=\"question-title header-5 text-unbold top colorLink\" href=\"(.*?)\" compid=\"title\">'
+	regex_question_urls = '<a class="question-title header-5 text-unbold top colorLink" href="(.*?)" compid="title">'
 	pattern_question_urls = re.compile(regex_question_urls)
 	quest_urls = re.findall(pattern_question_urls, htmltext)
 	question_urls.extend(quest_urls) 
-	#print(quest_urls)
 
-# #print questions[0:3]
-# #print question_urls[0:3]
-# #print ('start extracting answers')	
 
 answers = []
 #for url in question_urls: # get urls of one topic
 for url in question_urls: 
-	html = str(urllib.request.urlopen(url).read())
+	html = readURL(url)
 	soup = BeautifulSoup(html,'html.parser')
 	answerText = soup.select('div#questionDesc')[0].get_text() #soup.select() returns a list
-	#print(answerText)
 	answers.append(answerText)
-#print answers[0:3]
 
 
-#print(len(questions))
-#print(len(answers))
+# print(len(questions))
+# print(len(answers))
+# print(len(question_urls))
 for question, answer in zip(questions, answers):
 	print('<doc>')
 	print('  <field name="question">{0}</field>'.format(question))
 	print('  <field name="answer">{0}</field>'.format(answer))
 	print('</doc>')
-
-# '''
-# for category in urllist_h:
-# 	print("gettinng {category}.".format(category))
-# 	questions = 
-# 	for question in qeustions:
-# 		print("geting")
-# '''
